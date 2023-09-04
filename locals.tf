4dec19d7d9ffcf7ce9dba1508aac5d102fd77b3b
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-04 10:14:11 +0530

    terraform_vpc_infra

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   common_tags = {
     env = var.env
-    project = 'roboshop'
-    business_unit = 'ecommerce'
-    owner = 'ecommerce_robot'
+    project = "roboshop"
+    business_unit = "ecommerce"
+    owner = "ecommerce_robot"
   }
 }
