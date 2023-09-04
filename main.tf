fbca5bb82ba1f4c3135a965391926596e35e6d99
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-04 18:40:49 +0530

    terraform_vpc_infra

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_route_table" "public_route-table" {
 }
 
 resource "aws_route_table_association" "public_rt_association" {
-  count = length(aws_route_table.public_route-table)
+  count = length(aws_subnet.public_subnet)
   subnet_id = aws_subnet.public_subnet.*.id[count.index]
   route_table_id = aws_route_table.public_route-table.id
 }
@@ -115,7 +115,7 @@ resource "aws_route_table" "private_route-table" {
 }
 
 resource "aws_route_table_association" "private_rt_association" {
-  count = length(aws_route_table.private_route-table)
+  count = length(aws_subnet.private_subnet)
   subnet_id = aws_subnet.private_subnet.*.id[count.index]
   route_table_id = aws_route_table.private_route-table.id
 }
