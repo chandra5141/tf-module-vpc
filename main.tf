7a4f06a4716926f66cf2b4c1ad99ab8bf8636a2f
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-04 18:36:48 +0530

    terraform_vpc_infra

--- main.tf
+++ main.tf
@@ -7,23 +7,39 @@ resource "aws_vpc" "vpc" {
   )
 }
 
-resource "aws_subnet" "main" {
-  count      = length(var.subnets_cidr)
+
+resource "aws_subnet" "public_subnet" {
+  count      = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.subnets_cidr[count.index]
+  cidr_block = var.public_subnets_cidr[count.index]
 
   tags = merge(
     local.common_tags,{
-      Name = "${var.env}-subnets${count.index+1}"
+      Name = "${var.env}-public_subnet${count.index+1}"
     }
   )
 }
+
+
+resource "aws_subnet" "private_subnet" {
+  count      = length(var.private_subnets_cidr)
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.private_subnets_cidr[count.index]
+
+  tags = merge(
+    local.common_tags,{
+      Name = "${var.env}-private_subnet${count.index+1}"
+    }
+  )
+}
+
 
 resource "aws_vpc_peering_connection" "aws_vpc_connection" {
   peer_owner_id = data.aws_caller_identity.vpc_owner_id.account_id
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.vpc.id
   auto_accept = true
+
   tags = merge(
     local.common_tags,{
       Name = "${var.env}-vpc_peering_connection"
@@ -31,16 +47,40 @@ resource "aws_vpc_peering_connection" "aws_vpc_connection" {
   )
 }
 
-resource "aws_route" "default" {
-  route_table_id = aws_vpc.vpc.default_route_table_id
-  destination_cidr_block = "172.31.0.0/16"
-  vpc_peering_connection_id = aws_vpc_peering_connection.aws_vpc_connection.id
+
+resource "aws_route_table" "public_route-table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    cidr_block = data.aws_caller_identity.vpc_owner_id.id
+    vpc_peering_connection_id = aws_vpc_peering_connection.aws_vpc_connection.id
+  }
+
+  tags = merge(
+    local.common_tags,{
+      Name = "${var.env}-public_route_table"
+    }
+  )
 }
 
-resource "aws_route" "default-vpc" {
-  route_table_id = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block = var.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.aws_vpc_connection.id
+resource "aws_route_table_association" "public_rt_association" {
+  count = length(aws_route_table.public_route-table)
+  subnet_id = aws_subnet.public_subnet.*.id[count.index]
+  route_table_id = aws_route_table.public_route-table.id
+}
+
+resource "aws_eip" "eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "naw" {
+  subnet_id = aws_subnet.public_subnet.*.id[0]
+  allocation_id = aws_eip.eip.allocation_id
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -52,3 +92,31 @@ resource "aws_internet_gateway" "igw" {
     }
   )
 }
+
+
+resource "aws_route_table" "private_route-table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.naw.id
+  }
+
+  route {
+    cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.aws_vpc_connection.id
+  }
+
+  tags = merge(
+    local.common_tags,{
+      Name = "${var.env}-private_route_table"
+    }
+  )
+}
+
+resource "aws_route_table_association" "private_rt_association" {
+  count = length(aws_route_table.private_route-table)
+  subnet_id = aws_subnet.private_subnet.*.id[count.index]
+  route_table_id = aws_route_table.private_route-table.id
+}
+
