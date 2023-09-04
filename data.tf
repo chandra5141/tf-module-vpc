e2c8a2534e872c87a8a08077cdd9fdbd729b2ba8
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-04 13:06:14 +0530

    terraform_vpc_infra

--- data.tf
+++ data.tf
@@ -1,3 +1,4 @@
-data "aws_caller_identity" "vpc_owner_id" {
-
+data "aws_caller_identity" "vpc_owner_id" {}
+data "aws_vpc" "default" {
+  id = var.default_vpc_id
 }
--- main.tf
+++ main.tf
@@ -19,9 +19,9 @@ resource "aws_subnet" "main" {
   )
 }
 
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "aws_vpc_connection" {
   peer_owner_id = data.aws_caller_identity.vpc_owner_id.account_id
-  peer_vpc_id   = "vpc-024f86141cbbc02e6"
+  peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.vpc.id
   auto_accept = true
   tags = merge(
@@ -29,4 +29,16 @@ resource "aws_vpc_peering_connection" "foo" {
       Name = "${var.env}-vpc_peering_connection${count.index+1}"
     }
   )
+}
+
+resource "aws_route" "default" {
+  route_table_id = aws_vpc.vpc.default_route_table_id
+  destination_cidr_block = "172.31.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.aws_vpc_connection.id
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.aws_vpc_connection.id
 }
